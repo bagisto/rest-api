9aca038bc6e79f3ed2c161bc2687d5b18e238967
Devansh <devansh.bawari419@webkul.com>
2021-11-22 17:47:48 +0530

    Checking Laravel Sanctum And Session

---
+++ tests/BaseTestCase.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Webkul\RestApi\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class BaseTestCase extends TestCase
+{
+}
---
+++ tests/BasicTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Webkul\RestApi\Tests;
+
+use Webkul\RestApi\Tests\BaseTestCase;
+
+class BasicTest extends BaseTestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_basic_test()
+    {
+        $this->assertTrue(true);
+    }
+}
