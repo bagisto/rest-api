daf3ac164c4261859d99acb84dc6a577a15419f6
Devansh <devansh.bawari419@webkul.com>
2021-11-22 14:13:21 +0530

    Initial Commit

---
+++ src/Providers/RestApiServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Webkul\RestApi\Providers;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+class RestApiServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mapApiRoutes();
+    }
+
+    /**
+     * Define the "api" routes for the application.
+     *
+     * @return void
+     */
+    protected function mapApiRoutes()
+    {
+        Route::prefix('api')
+            ->middleware('api')
+            ->group(__DIR__ . '/../Routes/api.php');
+    }
+}
---
+++ src/Routes/api.php
@@ -0,0 +1,6 @@
+<?php
+
+/**
+ * APIs for version 1.
+ */
+require 'api-v1.php';
