c3958e373e4a573ad3eadbea40c759c92c3b0d25
Devansh <devansh.bawari419@webkul.com>
2022-06-08 16:18:56 +0530

    Admin Auth Fixed

--- src/Http/Controllers/V1/Admin/AdminController.php
+++ src/Http/Controllers/V1/Admin/AdminController.php
@@ -4,4 +4,19 @@ namespace Webkul\RestApi\Http\Controllers\V1\Admin;
 
 class AdminController extends ResourceController
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        /**
+         * This is for session based authentication. Activated to
+         * all the controllers which are inherit from this.
+         */
+        if (! request()->has('accept_token')) {
+            auth()->setDefaultDriver('admin');
+        }
+    }
 }
