eecfd430307aba8760df8323d5bd6f7ccca88588
Aqib-Webkul <aqibzaman.khan651@webkul.in>
2023-01-25 18:16:20 +0530

    Fixed Device Name not mandatory

--- src/Http/Controllers/V1/Admin/User/AuthController.php
+++ src/Http/Controllers/V1/Admin/User/AuthController.php
@@ -31,9 +31,10 @@ class AuthController extends UserController
         ]);
 
         if (! EnsureFrontendRequestsAreStateful::fromFrontend($request)) {
-            $request->validate([
-                'device_name' => 'required',
-            ]);
+            
+            if(!$request->device_name) {
+                $request->device_name = 'desktop';
+            }
 
             $admin = $adminRepository->where('email', $request->email)->first();
 
