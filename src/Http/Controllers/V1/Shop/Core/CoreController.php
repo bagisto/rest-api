7ee64103c739e6f2986331d0975a68aaaf8130e7
Vivek <viveksh047@webkul.in>
2023-05-15 13:26:55 +0530

    [Updated: config routes & resources added.]

--- src/Http/Controllers/V1/Shop/Core/CoreController.php
+++ src/Http/Controllers/V1/Shop/Core/CoreController.php
@@ -3,10 +3,42 @@
 namespace Webkul\RestApi\Http\Controllers\V1\Shop\Core;
 
 use Illuminate\Http\Request;
+use Webkul\Core\Repositories\CoreConfigRepository;
+use Webkul\RestApi\Http\Resources\V1\Shop\Core\ConfigurationResource;
 use Webkul\RestApi\Http\Controllers\V1\Shop\ShopController;
 
 class CoreController extends ShopController
 {
+    /**
+     * Is resource authorized.
+     *
+     * @return bool
+     */
+    public function isAuthorized()
+    {
+        return false;
+    }
+
+    /**
+     * Repository class name.
+     *
+     * @return string
+     */
+    public function repository()
+    {
+        return CoreConfigRepository::class;
+    }
+
+    /**
+     * Resource class name.
+     *
+     * @return string
+     */
+    public function resource()
+    {
+        return ConfigurationResource::class;
+    }
+    
     /**
      * Get core configs.
      *
