a54acdfbf529813a1c7953685474bd56a35fabee
amit kumar laravel <amitkumar.laravel358@webkul.in>
2023-05-17 17:05:08 +0530

    Update route method for post form data #18

--- src/Routes/V1/Shop/customer-routes.php
+++ src/Routes/V1/Shop/customer-routes.php
@@ -80,7 +80,7 @@ Route::group(['middleware' => ['auth:sanctum', 'sanctum.customer']], function ()
 
     Route::post('customer/cart/add/{productId}', [CartController::class, 'add']);
 
-    Route::put('customer/cart/update', [CartController::class, 'update']);
+    Route::post('customer/cart/update', [CartController::class, 'update']);
 
     Route::delete('customer/cart/remove/{cartItemId}', [CartController::class, 'removeItem']);
 
