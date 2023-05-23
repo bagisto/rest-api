e07307f88a0e7ed4f21a746a0899f90b4c639a05
Vivek <viveksh047@webkul.in>
2023-05-23 17:14:00 +0530

    [Updated slider resources]

--- src/Http/Resources/V1/Admin/Setting/SliderResource.php
+++ src/Http/Resources/V1/Admin/Setting/SliderResource.php
@@ -15,10 +15,19 @@ class SliderResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id'        => $this->id,
-            'title'     => $this->title,
-            'image_url' => $this->image_url,
-            'content'   => $this->content,
+            'id'            => $this->id,
+            'title'         => $this->title,
+            'path'          => $this->path,
+            'image_url'     => $this->image_url,
+            'content'       => $this->content,
+            'slider_path'   => $this->slider_path,
+            'channel_id'    => $this->channel_id,
+            'channel'       => new ChannelResource($this->channel),
+            'locale'        => $this->locale,
+            'sort_order'    => $this->sort_order,
+            'expired_at'    => $this->expired_at,
+            'created_at'    => $this->created_at,
+            'updated_at'    => $this->updated_at,
         ];
     }
 }
