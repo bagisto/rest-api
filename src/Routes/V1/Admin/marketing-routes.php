e8a07fe093802f98a9974062e682728aa07df35b
Shivendra <shivendra.gupta423@webkul.in>
2024-03-13 16:54:20 +0530

    Added newsletter subscription api

--- src/Routes/V1/Admin/marketing-routes.php
+++ src/Routes/V1/Admin/marketing-routes.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Webkul\RestApi\Http\Controllers\V1\Admin\Marketing\Communications\CampaignController;
 use Webkul\RestApi\Http\Controllers\V1\Admin\Marketing\Communications\EventController;
 use Webkul\RestApi\Http\Controllers\V1\Admin\Marketing\Communications\TemplateController;
+use Webkul\RestApi\Http\Controllers\V1\Admin\Marketing\Communications\SubscriptionController;
 use Webkul\RestApi\Http\Controllers\V1\Admin\Marketing\Promotions\CartRuleController;
 use Webkul\RestApi\Http\Controllers\V1\Admin\Marketing\Promotions\CartRuleCouponController;
 use Webkul\RestApi\Http\Controllers\V1\Admin\Marketing\Promotions\CatalogRuleController;
@@ -100,5 +101,31 @@ Route::group([
         Route::put('{id}', 'update');
 
         Route::delete('{id}', 'destroy');
+    });
+});
+
+/**
+ * Marketing routes.
+ */
+Route::group([
+    'middleware' => ['auth:sanctum', 'sanctum.admin'],
+    'prefix'     => 'marketing',
+], function () {
+    /**
+     * Communications routes.
+     */
+    Route::prefix('communications')->group(function () {
+        /**
+         * subscribers routes.
+         */
+        Route::controller(SubscriptionController::class)->prefix('subscribers')->group(function () {
+            Route::get('', 'allResources');
+
+            Route::get('{id}', 'getResource');
+
+            Route::put('{id}', 'update');
+
+            Route::delete('{id}', 'destroy');
+        });
     });
 });
